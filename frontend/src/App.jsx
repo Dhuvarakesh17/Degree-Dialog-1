db72ce73e3e6d60615f260acdeece384e15711b2
Dhuvarakesh17 <dhuvarakeshs92@gmail.com>
2025-04-18 14:08:13 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,4 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home/Home";
-// import Chat from "./pages/Chat/Chat";
-// import "./App.css";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/chat" element={<Chat />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Chat from "./pages/Chat/Chat";
 import "./App.css";
